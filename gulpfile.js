1c6d6226db073c6c629c2c353c76ec0bf9febac3
slaffko1 <malachit@gmail.com>
2016-01-13 20:24:15 +0200

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,10 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     jade = require('gulp-jade'),
-    //rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant'),
-    spritesmith = require('gulp.spritesmith'),
-    rimraf = require('rimraf');
-    //browserSync = require('browser-sync'),
-    //reload = browserSync.reload;
-    
-    //autoprefixer.Promise = require('bluebird');
+    spritesmith = require('gulp.spritesmith');
 
 var path = {
     build: {
@@ -29,8 +23,7 @@ var path = {
     },
     src: {
         html: 'src/jade/*.jade',
-        js: 'src/js/main.js',
-        jsplugins: 'src/js/plugins.js',
+        js: 'src/js/**/*.*',
         style: 'src/sass/style.sass',
         images: 'src/images/*.*',
         img: 'src/img/**/*.*',
@@ -47,48 +40,19 @@ var path = {
     clean: './build'
 };
 
-//var config = {
-//    server: {
-//        baseDir: "./build"
-//    },
-//    tunnel: true,
-//    host: 'localhost',
-//    port: 9000,
-//    logPrefix: "Frontend_Devil"
-//};
-//
-//gulp.task('webserver', function () {
-//    browserSync(config);
-//});
 
-gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
-});
 
 gulp.task('html:build', function () {
     gulp.src(path.src.html) 
         .pipe(jade({pretty: true}))
-        .pipe(gulp.dest(path.build.html))
-        //.pipe(reload({stream: true}));
+        .pipe(gulp.dest(path.build.html));
 });
 
 gulp.task('js:build', function () {
     gulp.src(path.src.js) 
-        //.pipe(rigger()) 
-        .pipe(sourcemaps.init()) 
+        .pipe(sourcemaps.init())
         .pipe(uglify()) 
-        //.pipe(sourcemaps.write()) 
-        .pipe(gulp.dest(path.build.js))
-        //.pipe(reload({stream: true}));
-});
-gulp.task('jsplugins:build', function () {
-    gulp.src(path.src.jsplugins) 
-        //.pipe(rigger()) 
-        .pipe(sourcemaps.init()) 
-        .pipe(uglify()) 
-        //.pipe(sourcemaps.write()) 
-        .pipe(gulp.dest(path.build.js))
-        //.pipe(reload({stream: true}));
+        .pipe(gulp.dest(path.build.js));
 });
 
 gulp.task('sprite:build', function () {
@@ -108,8 +72,7 @@ gulp.task('image:build', function () {
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.images))
-        //.pipe(reload({stream: true}));
+        .pipe(gulp.dest(path.build.images));
 });
 
 gulp.task('img:build', function() {
@@ -120,8 +83,7 @@ gulp.task('img:build', function() {
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest(path.build.img))
-        //.pipe(reload({stream: true}));
+        .pipe(gulp.dest(path.build.img));
 });
 
 gulp.task('style:build', function () {
@@ -138,15 +100,13 @@ gulp.task('style:build', function () {
         .pipe(prefixer())
         .pipe(cssmin())
         //.pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.style))
-        //.pipe(reload({stream: true}));
+        .pipe(gulp.dest(path.build.style));
 });
 
 
 gulp.task('build', [
     'html:build',
     'js:build',
-    'jsplugins:build',
     'style:build',
     'sprite:build',
     'img:build',
@@ -164,17 +124,8 @@ gulp.task('watch', function(){
     watch([path.watch.js], function(event, cb) {
         gulp.start('js:build');
     });
-    watch([path.watch.js], function(event, cb) {
-        gulp.start('jsplugins:build');
-    });
     watch([path.watch.img], function(event, cb) {
         gulp.start('sprite:build');
-    });    
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('image:build');
-    });
-    watch([path.watch.img], function(event, cb) {
-        gulp.start('img:build');
     });
 });
 
